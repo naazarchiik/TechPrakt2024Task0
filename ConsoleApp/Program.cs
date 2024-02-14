891f721c80cc5b1f41d4a25c94efad43b058d939
Nazarii Kryvolap <ipz222_kno@student.ztu.edu.ua>
2024-02-14 23:42:14 +0200

    gitignore & console app added to project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Введiть перше число:");
+            if (!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("Некоректний формат числа!");
+                return;
+            }
+
+            Console.WriteLine("Введiть друге число:");
+            if (!int.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("Некоректний формат числа!");
+                return;
+            }
+
+            double sum = number1 + number2;
+            Console.WriteLine($"Сума чисел {number1} та {number2} дорiвнює: {sum}");
+        }
+    }
+}
